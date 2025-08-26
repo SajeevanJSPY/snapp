c959460840bbe98a8a91283a9fb3247d57ccea38
sajeevan <sajeevanjspy@gmail.com>
2025-08-26 21:32:27 +0530

    chore: remove Prisma from project

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,18 +1,3 @@
-import { PrismaClient } from './generated/prisma';
-
-export const prisma = new PrismaClient();
-export * from './generated/prisma';
-export * from './prisma';
-
-// export the types as PascalNames
-export type {
-    usercontacts as UserContacts,
-    users as User1,
-    devices as Devices,
-    sessions as Session1,
-    device_status as DeviceStatus,
-} from './generated/prisma';
-
 export { User } from './users';
 export { Device } from './devices';
 export { Session } from './sessions';
--- packages/db/src/prisma/getCurrentUserInfo.ts
+++
@@ -1,6 +0,0 @@
-import { prisma, User } from '..';
-
-export async function getCurrentUserInfo(email: string): Promise<User | null> {
-    let data = await prisma.users.findUnique({ where: { email } })!;
-    return data;
-}
